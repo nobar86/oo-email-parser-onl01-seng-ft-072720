20286fc5ad2612afc0d7fc6dcca0cd943b09511c
Michael Rabon <nobar86@gmail.com>
2020-08-07 20:02:16 +0000

    Automatically backed up by Learn

---
+++ lib/email_parser.rb
@@ -0,0 +1,12 @@
+class EmailAddressParser
+  attr_accessor :name, :csv_emails
+  def initialize(csv_emails)
+    @csv_emails = csv_emails
+  end
+  def parse 
+    csv_emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end 
+end 
